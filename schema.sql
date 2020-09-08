837a4d8941974a929b89f65733c4276b2a87f5a4
peytonmford <peyton.m.ford@ttu.edu>
2020-09-08 16:51:19 -0500

    Added schema.

---
+++ schema.sql
@@ -0,0 +1,51 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+create table employees (
+	emp_no INT NOT NULL,
+	birth_date DATE not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	gender varchar not null,
+	hire_date date not null,
+	primary key (emp_no)
+);
+create table dept_manager (
+dept_no varchar(4) not null,
+	emp_no int not null,
+	from_date date not null,
+	to_date date not null,
+foreign key (emp_no) references employees (emp_no),
+foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+create table salaries (
+	emp_no int not null,
+	salary int not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no)
+);
+create table titles (
+	emp_no int not null,
+	title varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no)
+);
+create table dept_emp ( 
+	emp_no int not null,
+	dept_no varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+select * from departments;
